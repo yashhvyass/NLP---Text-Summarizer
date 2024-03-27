da0eddf2634adcbf48b6e326a1318c4a300a3bf7
Yash Vyas <yashvyas.indore@gmail.com>
2024-03-27 17:24:23 +0530

    Create FAST API, jinja2 template

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 import uvicorn
 import sys
@@ -6,7 +5,7 @@ import os
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from fastapi.responses import Response
-from textSummarizer.pipeline.prediction import PredictionPipeline
+from src.textSummarizer.pipeline.prediction import PredictionPipeline
 
 
 text:str = "What is Text Summarization?"
